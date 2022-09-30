cd2097c6d372e28b226bef9b136264a56869951b
DoMonkysFly7 <opreastefan9926@gmail.com>
2022-09-30 23:06:55 +0300

    Commit 2#

--- app.py
+++ app.py
@@ -39,6 +39,9 @@ db = SQL("sqlite:///adoption.db")
 # Forbidden characters global variable
 forbidden_characters = [";","?", "/","'","*","%",":","$","-","+",">","<","=","~","&","|", "#","^"]
 
+# Email characters
+email_characters = ["@", "."]
+
 @app.after_request
 def after_request(response):
     """Ensure responses aren't cached"""
@@ -56,7 +59,6 @@ def index():
     return render_template("index.html")
 
 
-
 @app.route("/register", methods=["GET", "POST"])
 def register():
     """Register user"""
@@ -64,9 +66,10 @@ def register():
     # Check user input
     if request.method == "POST":
 
-        # Get username & password
+        # Get username & password & confirmation & email
         username = request.form.get('username')
         password = request.form.get('password') 
+        confirmation = request.form.get('confirmation')
         email = request.form.get('email')
 
         # Validation against forbidden characters
@@ -96,13 +99,27 @@ def register():
             return apology("Email already in use", 400)
 
         # Password validation
+        if password != confirmation:
+            return apology("Passwords do not match", 400)
+        elif password == username:
+            return apology("Password and username must not match", 400)
+        
+        # Email validation
+        for i in range(len(email_characters)):
+            if email_characters[i] not in email:
+                return apology("Provide valid email address", 400)
 
         
+        # Generate password hash
+        pwhash = generate_password_hash(password)
+                
+        # Add user to database
+        db.execute("INSERT INTO users (username, hash, email) VALUES(?,?,?)", username, pwhash, email)
 
-        # Email validation
+        # Remember which user has logged in
+        new_user = db.execute("SELECT * FROM users WHERE username IS ?", username)
 
-
-        # Database insertion
+        session["user_id"] = new_user[0]["id"]
 
         # Set up automatic registration  mail
 
@@ -121,50 +138,56 @@ def register():
 
 
 
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
-#     """Log user in"""
+@app.route("/login", methods=["GET", "POST"])
+def login():
+    """Log user in"""
 
-#     # Forget any user_id
-#     session.clear()
+    # User reached route via POST (as by submitting a form via POST)
+    if request.method == "POST":
 
-#     # User reached route via POST (as by submitting a form via POST)
-#     if request.method == "POST":
+        # Get auth information
+        username = request.form.get('username')
+        password = request.form.get('password')
 
-#         # Ensure username was submitted
-#         if not request.form.get("username"):
-#             return apology("must provide username", 403)
+        # Ensure username/email was submitted
+        if not username:
+            return apology("Must provide username or email", 400)
 
-#         # Ensure password was submitted
-#         elif not request.form.get("password"):
-#             return apology("must provide password", 403)
+        # Ensure password was submitted
+        elif not password:
+            return apology("must provide password", 400)
 
-#         # Query database for username
-#         rows = db.execute("SELECT * FROM users WHERE username = ?", request.form.get("username"))
+        # Query database for username and email
+        rows = db.execute("SELECT * FROM users WHERE username = ?", username)
 
-#         # Ensure username exists and password is correct
-#         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], request.form.get("password")):
-#             return apology("invalid username and/or password", 403)
+         # Ensure username/email exists and password is correct
+        if (len(rows) != 1 or not check_password_hash(rows[0]["hash"], password)):
+            return apology("invalid username/email and/or password", 403)
 
-#         # Remember which user has logged in
-#         session["user_id"] = rows[0]["id"]
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
 
-#         # Redirect user to home page
-#         return redirect("/")
+        # Redirect user to home page
+        return redirect("/")
 
-#     # User reached route via GET (as by clicking a link or via redirect)
-#     else:
-#         return render_template("login.html")
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
-# @app.route("/logout")
-# def logout():
-#     """Log user out"""
 
-#     # Forget any user_id
-#     session.clear()
+@app.route("/logout")
+def logout():
+    """Log user out"""
 
-#     # Redirect user to login form
-#     return redirect("/")
+    # Forget any user_id
+    session.clear()
+
+    # Redirect user to login form
+    return redirect("/")
+
 
 if __name__ == '__main__':
+    print("I am being run directly")
     app.run(debug=True)
+else:
+    print("I am being imported")
